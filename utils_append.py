e24615c5c9752545d96d9c2150f416f7e7301933
geyao-c <1279846803@qq.com>
2022-04-13 14:53:37 +0800

    adapter23graf

--- utils_append.py
+++ utils_append.py
@@ -2267,6 +2267,90 @@ def graf_load_adapter22resnet_model(args, model, oristate_dict, layer, logger, n
 
     model.load_state_dict(state_dict)
 
+def graf_load_adapter23resnet_model(args, model, oristate_dict, layer, logger, name_base=''):
+    cfg = {
+        56: [9, 9, 9],
+        110: [18, 18, 18],
+    }
+
+    state_dict = model.state_dict()
+
+    current_cfg = cfg[layer]
+    last_select_index = None
+
+    all_conv_weight = []
+
+    prefix = args.ci_dir+'/ci_conv'
+    subfix = ".npy"
+
+    cnt = 1
+    for layer, num in enumerate(current_cfg):
+        layer_name = 'layer' + str(layer + 1) + '.'
+        for k in range(num):
+            # convnum = 0
+            for l in range(2):
+                cnt += 1
+                cov_id = cnt
+
+                conv_name = layer_name + str(k) + '.conv' + str(l + 1)
+                conv_weight_name = conv_name + '.weight'
+
+                all_conv_weight.append(conv_weight_name)
+                oriweight = oristate_dict[conv_weight_name]
+                curweight = state_dict[name_base+conv_weight_name]
+                orifilter_num = oriweight.size(0)
+                currentfilter_num = curweight.size(0)
+
+                logger.info(conv_weight_name)
+                if orifilter_num != currentfilter_num:
+                    logger.info('origilter num: {}, currentfilter num: {}'.format(orifilter_num, currentfilter_num))
+                    logger.info('loading ci from: ' + prefix + str(cov_id) + subfix)
+                    ci = np.load(prefix + str(cov_id) + subfix)
+                    logger.info('ci shape: {}'.format(ci.shape))
+                    logger.info('delta: {}'.format(orifilter_num - currentfilter_num))
+                    select_index = np.argsort(ci)[orifilter_num - currentfilter_num:]  # preserved filter id
+                    select_index.sort()
+                    logger.info(select_index)
+
+                    if last_select_index is not None:
+                        for index_i, i in enumerate(select_index):
+                            for index_j, j in enumerate(last_select_index):
+                                state_dict[name_base+conv_weight_name][index_i][index_j] = \
+                                    oristate_dict[conv_weight_name][i][j]
+                    else:
+                        for index_i, i in enumerate(select_index):
+                            state_dict[name_base+conv_weight_name][index_i] = \
+                                oristate_dict[conv_weight_name][i]
+
+                    last_select_index = select_index
+
+                elif last_select_index is not None:
+                    for index_i in range(orifilter_num):
+                        for index_j, j in enumerate(last_select_index):
+                            state_dict[name_base+conv_weight_name][index_i][index_j] = \
+                                oristate_dict[conv_weight_name][index_i][j]
+                    last_select_index = None
+
+                else:
+                    state_dict[name_base+conv_weight_name] = oriweight
+                    last_select_index = None
+
+    for name, module in model.named_modules():
+        name = name.replace('module.', '')
+
+        if isinstance(module, nn.Conv2d):
+            conv_name = name + '.weight'
+            if 'shortcut' in name:
+                continue
+            if conv_name not in all_conv_weight:
+                state_dict[name_base+conv_name] = oristate_dict[conv_name]
+
+        # elif isinstance(module, nn.Linear):
+        #     state_dict[name_base+name + '.weight'] = oristate_dict[name + '.weight']
+        #     state_dict[name_base+name + '.bias'] = oristate_dict[name + '.bias']
+
+    model.load_state_dict(state_dict)
+
 def graf_load_adapter1resnet_model(args, model, oristate_dict, layer, logger, name_base=''):
     cfg = {
         56: [9, 9, 9],
@@ -2463,6 +2547,9 @@ def load_arch_model(args, model, origin_model, ckpt, logger, graf=False):
             elif args.finetune_arch == 'adapter22resnet_56':
                 logger.info('graf_load_adapter22resnet_model')
                 graf_load_adapter22resnet_model(args, model, oristate_dict, 56, logger)
+            elif args.finetune_arch == 'adapter23resnet_56':
+                logger.info('graf_load_adapter23resnet_model')
+                graf_load_adapter23resnet_model(args, model, oristate_dict, 56, logger)
             else:
                 raise
         else:
