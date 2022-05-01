3f35fb88bf38d9eb8431eed738c59bfd9d912ccb
geyao-c <1279846803@qq.com>
2022-05-01 09:42:22 +0800

    tinyimagenet

--- data/tinyimagenet.py
+++ data/tinyimagenet.py
@@ -15,24 +15,24 @@ class Data:
         traindir = os.path.join(args.data_dir, 'train')
         valdir = os.path.join(args.data_dir, 'val')
         # imagenet mean and std
-        # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         # tinyimagenet mean and std
-        normalize = transforms.Normalize(mean=[0.4802, 0.4481, 0.3975], std=[0.2770, 0.2691, 0.2821])
+        # normalize = transforms.Normalize(mean=[0.4802, 0.4481, 0.3975], std=[0.2770, 0.2691, 0.2821])
         image_size = 64
         # 第一种变换方式
-        # train_transform = transforms.Compose([transforms.RandomResizedCrop(image_size), transforms.RandomHorizontalFlip(),
-        #                                       transforms.ToTensor(), normalize,])
-        # test_transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), normalize,])
+        train_transform = transforms.Compose([transforms.RandomResizedCrop(image_size), transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(), normalize,])
+        test_transform = transforms.Compose([transforms.Resize(image_size), transforms.ToTensor(), normalize,])
 
         print('image size is {}'.format(image_size))
         # 第二种变换方式
-        train_transform = transforms.Compose([
-            transforms.RandomCrop(image_size, padding=8),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            normalize
-        ])
-        test_transform = transforms.Compose([transforms.ToTensor(), normalize])
+        # train_transform = transforms.Compose([
+        #     transforms.RandomCrop(image_size, padding=8),
+        #     transforms.RandomHorizontalFlip(),
+        #     transforms.ToTensor(),
+        #     normalize
+        # ])
+        # test_transform = transforms.Compose([transforms.ToTensor(), normalize])
 
         trainset = datasets.ImageFolder(traindir, transform=train_transform)
         self.train_loader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True,
