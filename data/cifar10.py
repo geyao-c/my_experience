1884d5bc04e18a07e192434250f62cdfae6202d5
geyao-c <1279846803@qq.com>
2022-05-02 23:41:31 +0800

    mean and std xg

--- data/cifar10.py
+++ data/cifar10.py
@@ -13,10 +13,10 @@ def load_cifar_data(args):
     # normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     # cifar10的mean和std，这里的std有点问题
     # std = [0.24703225141799082, 0.24348516474564, 0.26158783926049628]
-    # normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
+    normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     # transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     # 流传的cifar10的std的正确版本
-    normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.262))
+    # normalize = transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.262))
 
     transform_train = transforms.Compose([
         transforms.RandomCrop(32, padding=4),
