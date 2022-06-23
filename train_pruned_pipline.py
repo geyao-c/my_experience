60b068b9b440cb198e3e037dccf59ad014e5e9ea
geyao-c <1279846803@qq.com>
2022-06-23 23:17:41 +0800

    xiaoxiu

--- train_pruned_pipline.py
+++ train_pruned_pipline.py
@@ -368,8 +368,8 @@ def main():
         end = time.time()
         logger.info("=>Best accuracy {:.3f} cost time is {:.3f}".format(best_top1_acc, (end - start)))
     logger.info("best top1 accu is {}, valid top1 accu is {}".format(best_top1_acc, valid_top1_acc))
-    best_top1_acc = round(best_top1_acc.data, 2)
-    valid_top1_acc = round(valid_top1_acc.data, 2)
+    best_top1_acc = round(best_top1_acc.item(), 2)
+    valid_top1_acc = round(valid_top1_acc.item(), 2)
     # 训练完成后会有两个模型精度，一个是最后一个模型，一个是验证集上精度最好的一个模型
     # 在这个精度范围内的模型才进行后续裁剪和微调操作
     pruned_finetune_pipline(args, now, best_top1_acc, best_accu_model)
