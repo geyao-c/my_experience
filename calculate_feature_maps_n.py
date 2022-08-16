1ede4279c712408ef41c969240b190586a3478e2
geyao-c <1279846803@qq.com>
2022-08-16 21:08:58 +0800

    xiugai

--- calculate_feature_maps_n.py
+++ calculate_feature_maps_n.py
@@ -14,6 +14,7 @@ from models.resnet_tinyimagenet import resnet_tinyimagenet_56
 from models.adapter_resnet_tinyimagenet import adapter3resnet_tinyimagenet_56
 from models.adapter_resnet_new_new import adapter8resnet_56
 from models.vgg_cifar10 import vgg_16_bn
+from models.selfsupcon_supcon_adapter_vgg import selfsupcon_supcon_adapter_vgg_16_bn
 from models.adapter_vgg_cifar10 import adapter_vgg_16_bn, adapter_vgg_16_bn_v4
 from models.adapter_resnet_new_three import adapter9resnet_56, adapter10resnet_56, adapter11resnet_56, \
     adapter12resnet_56, adapter13resnet_56, adapter14resnet_56, adapter15resnet_56, adapter17resnet_56, \
@@ -166,6 +167,19 @@ if args.arch=='vgg_16_bn':
         handler.remove()
 
 elif args.arch=='adapter_vgg_16_bn' or args.arch == 'adapter_vgg_16_bn_v4':
+
+    # if len(args.gpu) > 1:
+    #     relucfg = model.module.relucfg
+    # else:
+    relucfg = model.relucfg
+    start = time.time()
+    for i, cov_id in enumerate(relucfg):
+        cov_layer = model.features[cov_id]
+        handler = cov_layer.register_forward_hook(get_feature_hook)
+        inference()
+        handler.remove()
+
+elif args.arch=='selfsupcon_supcon_adapter_vgg_16_bn':
 
     # if len(args.gpu) > 1:
     #     relucfg = model.module.relucfg
