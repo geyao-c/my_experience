303397a0d4b3932cbb638fc275e6a007dfdf9f84
geyao-c <1279846803@qq.com>
2022-11-21 12:21:23 +0800

    xiugai

--- influence.py
+++ influence.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
         rcmd = 'python graf_prune_finetune_cifar_n.py --pretrained_dataset cifar10 --finetune_dataset cifar10 ' \
                '--finetune_data_dir ./data --pretrained_arch resnet_20 --finetune_arch resnet_20 --result_dir ' \
                './result/normal_pruned/92.21cifar10tocifar10_resnet_20_pruned_{} --ci_dir ./calculated_ci/92.21_resnet_20_cifar10 ' \
-               '--batch_size 128 --epochs 100 --lr_type cos --learning_rate 0.01 --momentum 0.9 --weight_decay 0.0005 ' \
+               '--batch_size 128 --epochs 150 --lr_type cos --learning_rate 0.01 --momentum 0.9 --weight_decay 0.0005 ' \
                '--graf --pretrain_dir ./pretrained_models/92.21_resnet_20_cifar10.pth.tar --sparsity [0.]*11+[{}]*1'.format(
             str(item), str(item))
 
@@ -49,7 +49,7 @@ if __name__ == '__main__':
         adrcmd = 'python graf_prune_finetune_cifar_n.py --pretrained_dataset cifar10 --finetune_dataset cifar10 ' \
                  '--finetune_data_dir ./data --pretrained_arch adapter15resnet_20 --finetune_arch adapter15resnet_20 --result_dir ' \
                  './result/normal_pruned/92.22cifar10tocifar10_adapter15resnet_20_pruned_{} --ci_dir ./calculated_ci/92.22_adapter15resnet_20_cifar10 ' \
-                 '--batch_size 128 --epochs 100 --lr_type cos --learning_rate 0.01 --momentum 0.9 --weight_decay 0.0005 ' \
+                 '--batch_size 128 --epochs 150 --lr_type cos --learning_rate 0.01 --momentum 0.9 --weight_decay 0.0005 ' \
                  '--graf --pretrain_dir ./pretrained_models/92.22_adapter15resnet_20_cifar10.pth.tar --sparsity [0.]*100 ' \
                  '--adapter_sparsity [{}]*1'.format(str(item), str(item))
 
