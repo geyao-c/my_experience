f0f9742ecbbcf191de0cfe63eef2ad5a3887772e
geyao-c <1279846803@qq.com>
2023-05-25 21:37:35 +0800

    xiugai

--- jt14.py
+++ jt14.py
@@ -60,8 +60,8 @@ def fun2():
     print('macs4/macs3: {}, params4/params3: {}'.format(1 - macs4 / macs3, 1 - params4 / params3))
 
 if __name__ == '__main__':
-    # fun1()
-    fun2()
+    fun1()
+    # fun2()
 
 # 278405196.0 3893206.0
 # 280051836.0 3918982.0
