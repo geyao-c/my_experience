2f021c84c144e31050f95f820a56be7cccba5483
geyao-c <1279846803@qq.com>
2023-11-24 19:48:24 +0800

    xiugai

--- models/resnet_imagenet.py
+++ models/resnet_imagenet.py
@@ -296,6 +296,8 @@ class ResNet50(nn.Module):
         return x
 
 def resnet_50(sparsity):
+    if sparsity is None:
+        sparsity = [0.0] * 100
     return ResNet50(sparsity=sparsity)
 
 def resnet_34(sparsity=None):
